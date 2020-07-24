e35f142190e58a7a6e73b373ba6b8727ac152dca
DKLeeAye <DKLeeAye@163.com>
2020-07-24 19:27:52 +0800

    RussiaCube0724

--- RussiaCubeDemo/Assets/Scripts/Block.cs
+++ RussiaCubeDemo/Assets/Scripts/Block.cs
@@ -8,53 +8,156 @@ public class Block : MonoBehaviour {
 	private bool[,] blockMatrix;
 	
 	private int size;
-	private float halfSize;
-	private float halfSizeFloat;
 	private float childSize;
 	private int xPosition;
 	private int yPosition;
 	private float fallSpeed;
-	private bool drop = false;
 	
 	// Use this for initialization
 	void Start () 
 	{
-
         size = block.Length;
 		int width = block[0].Length;
+		childSize = (size - 1) * .5f;		
+		blockMatrix = new bool[size, size];//块矩阵
 
-		halfSize = (size + 1) * .5f;
-		childSize = (size - 1) * .5f;//Size除以2再-0.5   方块0，1，2，3各自的相对位置计算   ！第0个子块的相对距离！！！
-		halfSizeFloat = size * .5f;
-		
-		blockMatrix = new bool[size, size];//块矩阵  二维布尔数组
-		for(int y=0;y<size;y++)
+		//方块生成
+		for (int y=0;y<size;y++)
 		{
-			for(int x=0;x<size;x++){
-				if (block[y][x] == '1'){
-				
+			for(int x=0;x<size;x++)
+			{
+				if (block[y][x] == '1')
+				{				
 					blockMatrix[y, x] = true;
 
 					//实例化一个cube并强制转化为Transform类型
 					//childSize - y（i）是坐标下移y（j）格 ，  x（j） - childSize为右移
-					var cube = (Transform)Instantiate(Manager.manager.cube, new Vector3(x - childSize, childSize - y, 0), Quaternion.identity);//实例化（复制）生成cube，设置坐标位置，默认角度
-					cube.parent = transform;////物体不受其他外力影响
-
+					var cube = (Transform)Instantiate(Manager.manager.cube, new Vector3(x - childSize, childSize - y, 0), Quaternion.identity);
+					cube.parent = transform;//物体不受其他外力影响
 				}
 			}
 		}
 
-			yPosition = Manager.manager.GetFieldHeight() - 1;
-			transform.position = new Vector3(Manager.manager.GetFieldWidth() / 2 + (size % 2 == 0 ? 0.5f : 0), yPosition - childSize, 0);
-			xPosition = (int)(transform.position.x - childSize);
+        yPosition = Manager.manager.GetFieldHeight() - 1;
+		transform.position = new Vector3( Manager.manager.GetFieldWidth() / 2 + (size % 2 == 0 ? 0.5f : 0), yPosition - childSize, 0);//方块初始位置
+		xPosition = (int)(transform.position.x - childSize);//方块中单个子块的世界坐标
+		fallSpeed = Manager.manager.blockNormalFallSpeed;
+				
+		//游戏结束
+		if (Manager.manager.CheckBlock(blockMatrix, xPosition, yPosition))//编号17 第18排是否有子块出现
+		{
+			Manager.manager.GameOver();
+			return;
+		}
+
+		StartCoroutine(CheckInput());
+		StartCoroutine(Delay((1 / Manager.manager.blockNormalFallSpeed) * 2));
+		StartCoroutine(Fall());
 	}
-	
 
 
-	// Update is called once per frame
-	void Update ()
+	IEnumerator Delay(float time)
 	{
-	
+		var t = 0f;
+		while (t <= time)
+		{
+			t += Time.deltaTime;
+			yield return null;
+		}
 	}
-	
+
+    //下落
+    IEnumerator Fall()
+	{
+		while(true)
+		{
+			//父块下落
+			for (float i = yPosition + 1;i > yPosition;i -= Time.deltaTime * fallSpeed)
+			{
+				transform.position = new Vector3(transform.position.x, i - childSize, transform.position.z);
+				yield return null;
+			}
+
+			//判断停止，Destroy并且SetBlock（）（销毁父块并在对应位置设置cube）
+			yPosition--;
+			if (Manager.manager.CheckBlock(blockMatrix, xPosition, yPosition))
+			{
+				Manager.manager.SetBlock(blockMatrix, xPosition, yPosition + 1);
+				Destroy(gameObject);
+				break;
+			}
+
+		}
+	}
+
+
+    //水平移动
+    IEnumerator MoveHorizontal(int distance)
+	{
+		
+		if (!Manager.manager.CheckBlock(blockMatrix, xPosition + distance, yPosition))
+		{
+			transform.position = new Vector3(transform.position.x + distance, transform.position.y, transform.position.z);
+			xPosition += distance;
+			yield return new WaitForSeconds(.1f);//延时
+		}
+		
+	}
+
+
+    //旋转方块
+    void RotateBlock(){
+		
+		var tempMatrix = new bool[size, size];//新建一布尔型数组
+
+	    for (int y = 0; y < size; y++)
+		{
+		     for (int x = 0; x < size; x++)
+			{
+		          tempMatrix[y, x] = blockMatrix[x, (size-1)-y];//旋转
+	         }
+		}
+		
+		if (!Manager.manager.CheckBlock(tempMatrix, xPosition, yPosition))//
+		{
+			System.Array.Copy(tempMatrix, blockMatrix, size * size);//将旋转后的数组复制过去
+			transform.Rotate(0, 0, 90);//父块旋转90度
+		}
+	}
+
+
+	//检测用户输入
+	IEnumerator CheckInput()
+	{
+		
+		while(true)
+		{
+			var input = Input.GetAxisRaw("Horizontal");
+			if (input < 0)
+			{
+				yield return StartCoroutine(MoveHorizontal(-1));
+			}
+			
+			if (input > 0)
+			{
+				yield return StartCoroutine(MoveHorizontal(1));
+			}
+			
+			if (Input.GetKeyDown(KeyCode.UpArrow))
+			{
+				RotateBlock();
+			}
+			
+			if (Input.GetKeyDown(KeyCode.DownArrow))
+			{
+				fallSpeed = Manager.manager.blockDropSpeed;
+				
+			}
+			
+			yield return null;
+		}
+		
+	}
+
+
 }
--- RussiaCubeDemo/Assets/Scripts/Manager.cs
+++ RussiaCubeDemo/Assets/Scripts/Manager.cs
@@ -5,95 +5,205 @@ public class Manager : MonoBehaviour {
 	
 	public GameObject[] blocks;
 	public Transform cube;
-	public Transform leftWall;
-	public Transform rightWall;
-	public int maxBlockSize = 4;
+	public Transform leftWall;//左边界
+	public Transform rightWall;//右边界
+	public int maxBlockSize = 4;//方块最大长度
 	public int _fieldWidth = 10;
 	public int _fieldHeight = 13;
+	public float blockNormalFallSpeed = 2f;
+	public float blockDropSpeed = 30f;
+
+	private int blockRandom;
 
 	private int fieldWidth;
 	private int fieldHeight;
 	private bool[,] fields;
+
 	private int[] cubeYposition;
 	private Transform[] cubeTransforms;
-	private int clearTimes;
-
-	private bool[,] blockMatrix;
-
-	private int blockRandom;
-	private GameObject nextBlock;
-	private Block nextB;
-	private int nextSize;
-	private string[] nextblock;
-
-
+	
 	public static Manager manager;//游戏管理对象manager
 
 	// Use this for initialization
 	void Start () 
 	{
+	
+		if (manager == null)
+		{
+			manager = this;
+		}
+	
 		blockRandom = Random.Range(0, blocks.Length);
 		
-		
-
-		fieldWidth = _fieldWidth + maxBlockSize * 2;
-		fieldHeight = _fieldHeight + maxBlockSize;
-		fields = new bool[fieldWidth, fieldHeight];//布尔型二维数组  游戏区域18*17
+		fieldWidth = _fieldWidth + maxBlockSize * 2;//游戏区域宽
+		fieldHeight = _fieldHeight + maxBlockSize;//游戏区域高   17
+		fields = new bool[fieldWidth, fieldHeight];//布尔型二维数组  游戏区域18*17 (10+4*2,13+4)
 		cubeYposition = new int[fieldHeight * fieldWidth];
 		cubeTransforms = new Transform[fieldHeight * fieldWidth];
 
-		for (int i = 0; i < fieldWidth; i++)
+		//两边边界布尔值全设为true，使方块无法出界
+		for (int i = 0;i < fieldHeight;i++)
+		{
+			
+			for (int j =0 ;j < maxBlockSize;j++)
+			{
+				
+				fields[j, i] = true;
+				fields[fieldWidth -1 - j, i] = true;
+				
+			}
+			
+		}
+		
+		//底部边界
+		for (int i=0;i<fieldWidth;i++)
 		{
 			fields[i, 0] = true;
 		}
-
-
-		CreateBlock(blockRandom);//随机创建方块
-
-	
+		
+		CreateBlock(blockRandom);//创建方块
 	}
-
+	
 	void CreateBlock(int random)
 	{
 		Instantiate(blocks[random]);//实例化对象
 		blockRandom = Random.Range(0, blocks.Length);//随机数，块的序号
-		nextBlock = blocks[blockRandom];//随机块布尔数组生成
-		nextB = (Block)nextBlock.GetComponent("Block");//获取nextBlock的Block组件，强制转化成block型
-		nextSize = nextB.block.GetLength(0);//获得第一维的长度
-		nextblock = new string[nextSize];//新建长度相同的字符串
-		nextblock = nextB.block;//储存第一维的字符串 例如0000
 	}
-
+	
 	public int GetFieldWidth()
 	{
 		return fieldWidth;
 	}
-
+	
 	public int GetFieldHeight()
 	{
 		return fieldHeight;
 	}
-
+	
 	public int GetBlockRandom()
 	{
 		return blockRandom;
 	}
+	
+	//检查子块
+	public bool CheckBlock(bool [,] blockMatrix, int xPos, int yPos)
+	{
+		var size = blockMatrix.GetLength(0);//第0行长度，数组维数
 
+		for (int y = 0;y < size;y++)
+		{
+			for (int x = 0;x < size;x++)
+			{
+				if (blockMatrix[y, x] && fields[xPos + x, yPos - y])//y=0，yPos=17，检查第18行是否有子块  fields与blockMatrix的对比  
+				{
+					return true;
+				}
+			}
+		}	
+		return false;
+	}
+	
+	//将块停留的位置bool区域矩阵设为true，并摆放单个方块
 	public void SetBlock(bool[,] blockMatrix, int xPos, int yPos)
 	{
-
-		int size = blockMatrix.GetLength(0);//第一行长度
-		for (int y = 0; y < size; y++)
+		
+		int size = blockMatrix.GetLength(0);//第0行长度
+		for (int y = 0;y < size;y++)
 		{
-			for (int x = 0; x < size; x++)
+			for (int x = 0;x < size;x++)
 			{
 				if (blockMatrix[y, x])
 				{
-					Instantiate(cube, new Vector3(xPos + x, yPos - y, 0), Quaternion.identity);
+					Instantiate(cube, new Vector3(xPos + x, yPos - y, 0), Quaternion.identity);//还原子块
+					fields[xPos + x, yPos - y] = true;//游戏区域数组对应位置设为true
 				}
 			}
-		}		
+		}
+		StartCoroutine(CheckRows(yPos - size, size));
+		
 	}
+	
+	
+	//是否成行
+	IEnumerator CheckRows(int yStart, int size)
+	{
+		yield return null;
+		if (yStart < 1)
+		{
+			yStart = 1;
+		}
+		//int count = 1;
+		for (int y = yStart;y < yStart + size;y++)
+		{
+			int x;
+			for (x = maxBlockSize;x < fieldWidth - maxBlockSize;x++)//找到该行一个子块X就+1
+			{
+				if (!fields[x, y])
+				{
+					break;
+				}
+			}
+			if (x == fieldWidth - maxBlockSize)//行满
+			{
+				yield return StartCoroutine(SetRows(y));
+				y--;//行消除所以y-1
+				//count++;
+			}
+		}
+		CreateBlock(blockRandom);//再次创建
+	}
+	
 
-
+	IEnumerator SetRows(int yStart)
+	{
+		//游戏区域布尔数组值依次下降一行
+		for (int y = yStart;y < fieldHeight - 1;y++)
+		{
+			for (int x = maxBlockSize;x < fieldWidth - maxBlockSize;x++)
+			{
+				fields[x, y] = fields[x, y + 1];
+			}
+		}
+		//下降完最上面一行设为false
+		for (int x = maxBlockSize;x < fieldWidth - maxBlockSize;x++)
+		{
+			fields[x, fieldHeight - 1] = false;
+		}
+		
+		var cubes = GameObject.FindGameObjectsWithTag("Cube");//cube设置标签
+		int cubeToMove = 0;
+		for (int i = 0;i < cubes.Length;i++)
+		{
+			GameObject cube = cubes[i];
+			if (cube.transform.position.y > yStart)
+			{
+				cubeYposition[cubeToMove] = (int)(cube.transform.position.y);
+				cubeTransforms[cubeToMove++] = cube.transform;
+			}//需要下移一行的cube
+			else if (cube.transform.position.y == yStart)
+			{
+				Destroy(cube);
+			}//需要销毁的cube
+		}
+		
+		float t = 0;
+		while (t <= 1f)
+		{
+			t += Time.deltaTime * 5f;
+			for(int i = 0;i < cubeToMove;i++)
+			{
+				cubeTransforms[i].position = new Vector3(cubeTransforms[i].position.x, Mathf.Lerp(cubeYposition[i], cubeYposition[i] - 1, t),
+					cubeTransforms[i].position.z);//方块下落的时间过渡
+			}
+		    yield return null;
+		}
+		
+		
+		
+	}
+	
+	public void GameOver()
+	{
+		print("Game Over!!!");
+	}
 }
