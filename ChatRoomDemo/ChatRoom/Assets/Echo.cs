812de14aa9b5d5de4c7db18b471be3a94f4cdb82
DKLeeAye <DKLeeAye@163.com>
2020-07-27 18:36:54 +0800

    0727ChatRoomCode

---
+++ ChatRoomDemo/ChatRoom/Assets/Echo.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Net.Sockets;//Socket编程的API位于此命名空间中
+using UnityEngine.UI;
+
+public class Echo : MonoBehaviour
+{
+	Socket sokect;
+	public InputField inputField;
+	public Text text;
+
+	public void Connection()
+    {
+		//Socket 网络连接的端点
+		sokect = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);//创建一个Socket对象，三个参数代表地址族、套接字类型、协议
+		sokect.Connect("127.0.0.1", 8888);//连接服务端  Connect是一个阻塞方法，程序会卡住直到服务端回应（接收，拒绝，超时）
+    }
+
+	public void Send()
+    {
+		//发送
+		string sendStr = inputField.text;
+		byte[] sendBytes = System.Text.Encoding.Default.GetBytes(sendStr);//字符串转换为byte[]数组
+		sokect.Send(sendBytes);//发送数据  阻塞办法  返回值指明发送数据的长度
+
+		//接受
+		byte[] readBuff = new byte[1024];
+		int count = sokect.Receive(readBuff);//接收消息 阻塞方法 其中byte[]类型的储存接收到的数据  返回值指明接收数据的长度
+		string recvStr = System.Text.Encoding.Default.GetString(readBuff, 0, count);//byte[]转换成字符串
+
+		text.text = recvStr;
+
+		//关闭
+		sokect.Close();//关闭连接
+    }
+}
---
+++ ChatRoomDemo/Server/Server/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Server
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //socket
+            Socket listenfd = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            //Bind
+            IPAddress ipAdr = IPAddress.Parse("127.0.0.1");//回送地址127.0.0.1 指本地机，一般用于测试
+            IPEndPoint ipEp = new IPEndPoint(ipAdr, 8888);//8888端口  指定IP与端口
+            listenfd.Bind(ipEp);//给listenfd套接字绑定IP和端口
+
+            //listen
+            listenfd.Listen(0);//开启监听，等待客户端连接 参数backlog指定队列中最多可容纳等待接受的连接数 0代表不限制
+            Console.WriteLine("服务器启动成功");
+
+            while (true)
+            {
+                //接受
+                Socket connfd = listenfd.Accept();//接受客户端连接  阻塞办法 连接前都会卡死 返回一个新客户端的socket对象connfd用来处理该客户端的数据
+                Console.WriteLine("服务器接受");
+
+                //接收
+                byte[] readBuff = new byte[1024];
+                int count = connfd.Receive(readBuff);
+                string readStr = System.Text.Encoding.Default.GetString(readBuff, 0, count);
+                Console.WriteLine("服务器接收" + readStr);
+
+                //发送
+                byte[] sendBytes = System.Text.Encoding.Default.GetBytes(readStr);
+                connfd.Send(sendBytes);
+            }
+
+        }
+    }
+}
