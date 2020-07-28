edf0b34f35f62a405ad7d271c711e5b6e7018a5b
DKLeeAye <DKLeeAye@163.com>
2020-07-28 18:38:00 +0800

    0728ChatRoomCode

---
+++ ChatRoomDemo/ChatRoom/Assets/Scripts/Echo.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Net.Sockets;//Socket编程的API位于此命名空间中
+using UnityEngine.UI;
+using System;
+
+public class Echo : MonoBehaviour
+{
+	Socket socket;
+	public InputField inputField;
+	public Text text;
+	public string userName;
+
+	//接收缓冲区
+	byte[] readBuff = new byte[1024];
+	string recvStr = "";
+
+
+
+	public void Connection()
+	{
+		//Socket 网络连接的端点
+		socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);//创建一个Socket对象，三个参数代表地址族、套接字类型、协议
+
+		socket.BeginConnect("127.0.0.1", 8888, ConnectCallback, socket);//连接服务端  Connect是一个阻塞方法，程序会卡住直到服务端回应（接收，拒绝，超时）
+	}
+
+	//Connect回调
+	public void ConnectCallback(IAsyncResult ar) //异步委托类ar
+	{
+		try
+		{
+			Socket socket = (Socket)ar.AsyncState;//ar.AsyncState获取BeginConnect传入的最后一个表示连接状态的参数
+			socket.EndConnect(ar);
+			Debug.Log("Socket连接成功");
+			socket.BeginReceive(readBuff, 0, 1024, 0, ReceiveCallback, socket);
+		}
+		catch (SocketException ex)//异常发生时执行Catch中的代码，打印ex中附带的异常信息
+		{
+			Debug.Log("Socket连接失败" + ex.ToString());
+		}
+	}
+
+	//Receive回调
+	public void ReceiveCallback(IAsyncResult ar)
+    {
+        try
+        {
+			Socket socket = (Socket)ar.AsyncState;
+			int count = socket.EndReceive(ar);
+			//recvStr = System.Text.Encoding.Default.GetString(readBuff,0,count);
+			//接收历史消息
+			string s = System.Text.Encoding.Default.GetString(readBuff, 0, count);
+			recvStr = recvStr + "\n" + s ;
+
+			socket.BeginReceive(readBuff, 0, 1024, 0, ReceiveCallback, socket);
+        }
+		catch(SocketException ex)
+        {
+			Debug.Log("Socket 接收失败" + ex.ToString());
+        }
+    }
+
+	public void Send()
+	{
+		//发送
+		string sendStr = userName +": " + inputField.text;
+		byte[] sendBytes = System.Text.Encoding.Default.GetBytes(sendStr);//字符串转换为byte[]数组
+		socket.BeginSend(sendBytes,0,sendBytes.Length,0,SendCallback,socket);
+	}
+
+	//Send 回调
+	public void SendCallback(IAsyncResult ar)
+    {
+        try
+        {
+			Socket socket = (Socket)ar.AsyncState;
+			int count = socket.EndSend(ar);
+			Debug.Log("Socket发送成功" + count);
+        }
+        catch (SocketException ex)
+        {
+			Debug.Log("Socket发送失败" + ex.ToString());
+        }
+    }
+
+	public void Update()
+    {
+		text.text = recvStr;		
+	}
+}
--- ChatRoomDemo/Server/Server/Program.cs
+++ ChatRoomDemo/Server/Server/Program.cs
@@ -1,16 +1,24 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net;
 using System.Net.Sockets;
 
-namespace Server
-{
+    class ClientState   //客户端类  存储客户端的一些信息
+    {
+        public Socket socket;
+        public byte[] readBuff = new byte[1024];
+        public string userName = "";
+    }
     class Program
     {
+        static Socket listenfd;
+        
+    //客户端Socket及状态信息
+        static Dictionary<Socket, ClientState> clients = new Dictionary<Socket, ClientState>();
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            //socket
-            Socket listenfd = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            //Socket
+            listenfd = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
 
             //Bind
             IPAddress ipAdr = IPAddress.Parse("127.0.0.1");//回送地址127.0.0.1 指本地机，一般用于测试
@@ -21,23 +29,65 @@ namespace Server
             listenfd.Listen(0);//开启监听，等待客户端连接 参数backlog指定队列中最多可容纳等待接受的连接数 0代表不限制
             Console.WriteLine("服务器启动成功");
 
-            while (true)
+            //接受
+            listenfd.BeginAccept(AcceptCallback, listenfd);
+            //等待
+            Console.ReadLine();
+        }
+
+        public static void AcceptCallback(IAsyncResult ar)
+        {
+            try
             {
-                //接受
-                Socket connfd = listenfd.Accept();//接受客户端连接  阻塞办法 连接前都会卡死 返回一个新客户端的socket对象connfd用来处理该客户端的数据
-                Console.WriteLine("服务器接受");
+                Console.WriteLine("服务器Accept");
+                Socket listenfd = (Socket)ar.AsyncState;
+                Socket clientfd = listenfd.EndAccept(ar);//新客户端的socket
+                //clients列表
+                ClientState state = new ClientState();
+                state.socket = clientfd;
+                clients.Add(clientfd, state);
 
-                //接收
-                byte[] readBuff = new byte[1024];
-                int count = connfd.Receive(readBuff);
-                string readStr = System.Text.Encoding.Default.GetString(readBuff, 0, count);
-                Console.WriteLine("服务器接收" + readStr);
-
-                //发送
-                byte[] sendBytes = System.Text.Encoding.Default.GetBytes(readStr);
-                connfd.Send(sendBytes);
+                //接收数据
+                clientfd.BeginReceive(state.readBuff, 0, 1024, 0, ReceiveCallback, state);
+                //继续Accept
+                listenfd.BeginAccept(AcceptCallback, listenfd);
             }
+            catch(SocketException ex)
+            {
+                Console.WriteLine("Socket接收失败" + ex.ToString());
+            }
+        }
 
+        public static void ReceiveCallback(IAsyncResult ar) 
+        {
+            try
+            {
+                ClientState state = (ClientState)ar.AsyncState;
+                Socket clientfd = state.socket;
+                int count = clientfd.EndReceive(ar);
+                //客户端关闭
+                if(count == 0 )
+                {
+                    clientfd.Close();
+                    clients.Remove(clientfd);
+                    Console.WriteLine("Socket close");
+                    return;
+                }
+
+                string recvStr = System.Text.Encoding.Default.GetString(state.readBuff, 0, count);
+                byte[] sendBytes = System.Text.Encoding.Default.GetBytes(recvStr);
+                //clientfd.Send(sendBytes);
+                //遍历所有用户，发送信息
+                foreach (ClientState s in clients.Values)
+                {
+                s.socket.Send(sendBytes);//这里暂时未使用异步发送
+                }
+
+                clientfd.BeginReceive(state.readBuff, 0, 1024, 0, ReceiveCallback, state);
+            }
+            catch(SocketException ex)
+            {
+                Console.WriteLine("Socket接收失败" + ex.ToString());
+            }
         }
     }
-}
